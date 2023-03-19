cf745655bd7c195d81e238679883450df24f5fce
Tej Chajed <tchajed@gmail.com>
2023-03-19 16:19:32 -0700

    Add a basic HTML+CSS timer
    Heavily based on Jon's timer <https://jon.tsp.io/time/> for now, but
    now we can make some tweaks.

--- src/greet.ts
+++
@@ -1,3 +0,0 @@
-export function sayHello(compiler: string) {
-  return `Hello from ${compiler}`;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,2 @@
-import { sayHello } from "./greet";
-
-function showHello(divName: string, name: string) {
-  const elt = document.getElementById(divName);
-  if (elt == null) {
-    console.error(`${divName} not found`);
-    return;
-  }
-  elt.innerText = sayHello(name);
-}
-
-showHello("greeting", "TypeScript");
+const lang = "typescript";
+console.log(`runnin' some ${lang}`);
