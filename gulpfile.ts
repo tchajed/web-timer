57420c6a7bacc3fce9d47c69722362bc76ed8528
Tej Chajed <tchajed@gmail.com>
2023-03-19 14:52:31 -0700

    Add a working less compilation+serving setup

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,14 +1,12 @@
 import browserify from "browserify";
 import fancy_log from "fancy-log";
 import gulp from "gulp";
+import less from "gulp-less";
+import path from "path";
 import tsify from "tsify";
 import source from "vinyl-source-stream";
 import watchify from "watchify";
-const server: any = require("gulp-server-livereload");
-
-const paths = {
-  pages: ["src/*.html"],
-}
+import browserSync from "browser-sync";
 
 const watchedBrowserify = watchify(
   browserify({
@@ -21,9 +19,20 @@ const watchedBrowserify = watchify(
     .plugin(tsify)
 );
 
+const paths = {
+  pages: ["src/*.html"],
+}
 gulp.task("copy-html", () => {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 })
+
+gulp.task("styles", () => {
+  return gulp.src("./styles/*.less")
+    .pipe(less({
+      paths: [path.join(__dirname, "less", "includes")]
+    }))
+    .pipe(gulp.dest("./dist/css"));
+});
 
 function bundle() {
   return watchedBrowserify
@@ -33,15 +42,19 @@ function bundle() {
     .pipe(gulp.dest("dist"));
 }
 
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+gulp.task("default", gulp.series(gulp.parallel("copy-html", "styles"), bundle));
 watchedBrowserify.on("update", bundle);
 watchedBrowserify.on("log", fancy_log);
 
-gulp.task("webserver", () => {
-  gulp.src(".")
-    .pipe(server({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+gulp.task("serve", () => {
+  browserSync({
+    server: {
+      baseDir: "dist",
+    }
+  })
+  gulp.watch(
+    ["dist/*.html", "dist/css/*.css", "dist/*.js"],
+    { cwd: "dist" },
+    browserSync.reload,
+  );
 });
