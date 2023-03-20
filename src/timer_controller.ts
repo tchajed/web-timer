1f38cbe3d3c59bbebecb13eb3b7d196240b7c6fc
Tej Chajed <tchajed@gmail.com>
2023-03-19 22:26:48 -0700

    Add support for hours

--- src/timer_controller.ts
+++ src/timer_controller.ts
@@ -1,15 +1,21 @@
 import { Timer, State } from "./timer";
 
 const timer = new Timer();
-const dom = {
-  counters: document.getElementById("counters"),
-  min: document.getElementById("min"),
-  sec: document.getElementById("sec"),
-  start_stop_btn: document.getElementById("start_stop"),
-  reset_btn: document.getElementById("reset"),
+function getElementById(id: string): HTMLElement {
+  const el = document.getElementById(id);
+  if (el == null) {
+    throw new Error(`dom element ${id} not found`);
+  }
+  return el;
 }
-
-var intervalId: ReturnType<typeof setInterval> = null;
+const dom = {
+  counters: getElementById("counters"),
+  hr: getElementById("hr"),
+  min: getElementById("min"),
+  sec: getElementById("sec"),
+  start_stop_btn: getElementById("start_stop"),
+  reset_btn: getElementById("reset"),
+}
 
 // pad s to be at least minLength, by adding copies of padString to the start
 function padStart(s: string, minLength: number, padString: string): string {
@@ -22,37 +28,61 @@ function padStart(s: string, minLength: number, padString: string): string {
 
 function updateTime() {
   const totalSec = timer.secondsElapsed();
-  const min = Math.round(totalSec / 60);
+  const hr = Math.round(totalSec / 3600);
+  const min = Math.round((totalSec % 3600) / 60);
   const sec = Math.round(totalSec % 60);
-  dom.min.innerText = `${min}`;
-  dom.sec.innerText = padStart(`${sec}`, 2, "0");
-}
-
-function stopUpdateInterval() {
-  if (intervalId != null) {
-    clearInterval(intervalId);
-    intervalId = null;
+  if (hr > 0) {
+    dom.counters.classList.add("hr");
+    dom.counters.classList.remove("min");
+    dom.hr.textContent = `${hr}`;
+    dom.min.textContent = padStart(`${min}`, 2, "0");
+  } else {
+    dom.counters.classList.add("min");
+    dom.counters.classList.remove("hr");
+    dom.min.textContent = `${min}`;
+    dom.sec.textContent = padStart(`${sec}`, 2, "0");
   }
 }
+
+class Updater {
+  cb: () => any;
+  intervalId: ReturnType<typeof setInterval>;
+
+  constructor(cb: () => any) {
+    this.cb = cb;
+    this.intervalId = null;
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  start() {
+    if (this.intervalId == null) {
+      this.intervalId = setInterval(this.cb, 200);
+    }
+  }
+}
+
+const updater = new Updater(updateTime);
 
 function updateDom() {
   if (timer.state == State.Running) {
     dom.counters.classList.add("running");
-    if (intervalId == null) {
-      intervalId = setInterval(updateTime, 200);
-    }
+    updater.start();
     dom.start_stop_btn.innerText = "pause";
   } else if (timer.state == State.NotStarted) {
-    stopUpdateInterval();
+    updater.stop();
     dom.counters.classList.remove("running");
     dom.start_stop_btn.innerText = "start";
   } else {
     // timer.state == State.Paused
-    stopUpdateInterval();
-    updateTime();
+    updater.stop();
     dom.counters.classList.remove("running");
     dom.start_stop_btn.innerText = "resume";
   }
+  updateTime();
 }
 
 function start() {
@@ -70,6 +100,8 @@ function reset() {
   updateDom();
   updateTime();
 }
+
+updateDom();
 
 const controller = {
   timer,
